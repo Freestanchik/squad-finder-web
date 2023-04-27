848f5dc3303fcd942e286be7d44687c1ac724033
Oleksandr Bihun <bihun.sasha2019@gmail.com>
2023-04-27 15:27:19 +0300

    Add header and footer

---
+++ client/src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import styles from "../footer/styles.module.scss";
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+           <p>Bihun Sasha web app</p>
+        </footer>
+    );
+};
+
+export default Footer
---
+++ client/src/components/header/Header.tsx
@@ -0,0 +1,26 @@
+import React, {Fragment} from 'react';
+import logo from '../../assets/logo.png'
+import styles from './styles.module.scss'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faUser} from '@fortawesome/free-regular-svg-icons';
+
+const Header = () => {
+    return (
+            <header className={styles.header}>
+                <div className={styles.container}>
+                    <div className={styles.header__logo}>
+                        <img className={styles.logo} src={logo} alt="Squad finder web logo"/>
+                        <p className={styles.app_name}>SQUAD FINDER</p>
+                    </div>
+                    <div className={styles.header__user_info}>
+                        <p className={styles.user_name}>Guest</p>
+                        <span className={styles.profile_icon}>
+                            <FontAwesomeIcon icon={faUser}/>
+                        </span>
+                    </div>
+                </div>
+            </header>
+    );
+};
+
+export default Header;
